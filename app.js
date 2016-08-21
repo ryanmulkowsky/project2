4b2ebc0e190aa86ca3ec9a3e6c1abbf3e7b2acaa
Ryan Mulkowsky <ryanmulkowsky@gmail.com>
2016-08-21 18:07:07 -0400

    Created Passport Branch. Created Authorization and Authentication. Created User Model.

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 var bluebird = require('bluebird');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -32,6 +35,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(session({ secret: 'WDI Rocks!',
+                  resave: true,
+                  saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport/passport')(passport);
+
+// This middleware will allow us to use the currentUser in our views and routes.
+app.use(function (req, res, next) {
+  global.currentUser = req.user;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/filecabinet.js
+++ routes/filecabinet.js
@@ -9,44 +9,59 @@ function makeError(res, message, status) {
   return error;
 }
 
+function authenticate(req, res, next) {
+  if(!req.isAuthenticated()) {
+    req.flash('error', 'Please signup or login.');
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
+/*
 // INDEX
-router.get('/', function(req, res, next) {
+router.get('/', authenticate, function(req, res, next) {
   FileCabinet.find({})
   .then(function(fileCabinet) {
     res.render('filecabinet/index', { fileCabinet: fileCabinet });
   });
 });
+*/
+
+// INDEX
+router.get('/', authenticate, function(req, res, next) {
+  var fileCabinet = global.currentUser.fileCabinet;
+  res.render('filecabinet/index', { fileCabinet: fileCabinet, message: req.flash() });
+});
 
 // NEW
-router.get('/new', function(req, res, next) {
+router.get('/new', authenticate, function(req, res, next) {
   var fileCabinet = {
     title: '',
     category: '',
     link: '',
   };
-  res.render('filecabinet/new', { fileCabinet: fileCabinet } );
+  res.render('filecabinet/new', { fileCabinet: fileCabinet, message: req.flash() });
 });
 
 // SHOW
-router.get('/:id', function(req, res, next) {
-  FileCabinet.findById(req.params.id)
-  .then(function(fileCabinet) {
+router.get('/:id', authenticate, function(req, res, next) {
+    var fileCabinet = currentUser.fileCabinet.id(req.params.id);
     if (!fileCabinet) return next(makeError(res, 'Document not found', 404));
-    res.render('filecabinet/show', { fileCabinet: fileCabinet });
-  }, function(err) {
-    return next(err);
-  });
+    res.render('filecabinet/show', { fileCabinet: fileCabinet, message: req.flash() });
 });
 
 // CREATE
-router.post('/', function(req, res, next) {
+router.post('/', authenticate, function(req, res, next) {
   var fileCabinet = new FileCabinet({
     title: req.body.title,
     category: req.body.category,
     link: req.body.link
   });
-  fileCabinet.save()
-  .then(function(saved) {
+  currentUser.fileCabinet.push(fileCabinet);
+  currentUser.save()
+  .then(function() {
     res.redirect('/filecabinet');
   }, function(err) {
     return next(err);
@@ -54,37 +69,37 @@ router.post('/', function(req, res, next) {
 });
 
 // EDIT
-router.get('/:id/edit', function(req, res, next) {
-  FileCabinet.findById(req.params.id)
-  .then(function(fileCabinet) {
+router.get('/:id/edit', authenticate, function(req, res, next) {
+    var fileCabinet = currentUser.fileCabinet.id(req.params.id);
     if (!fileCabinet) return next(makeError(res, 'Document not found', 404));
-    res.render('filecabinet/edit', { fileCabinet: fileCabinet });
-  }, function(err) {
-    return next(err);
-  });
+    res.render('filecabinet/edit', { fileCabinet: fileCabinet, message: req.flash() });
 });
 
 // UPDATE
-router.put('/:id', function(req, res, next) {
-  FileCabinet.findById(req.params.id)
-  .then(function(fileCabinet) {
-    if (!fileCabinet) return next(makeError(res, 'Document not found', 404));
+router.put('/:id', authenticate, function(req, res, next) {
+  var fileCabinet = currentUser.fileCabinet.id(req.params.id);
+  if (!fileCabinet) return next(makeError(res, 'Document not found', 404));
+  else {
     fileCabinet.title = req.body.title;
     fileCabinet.category = req.body.category;
     fileCabinet.link = req.body.link;
-    return fileCabinet.save();
-  })
-  .then(function(saved) {
-    res.redirect('/filecabinet');
-  }, function(err) {
-    return next(err);
-  });
+    currentUser.save()
+    .then(function(saved) {
+      res.redirect('/filecabinet');
+    }, function(err) {
+      return next(err);
+    });
+  }
 });
 
 // DESTROY
-router.delete('/:id', function(req, res, next) {
-  FileCabinet.findByIdAndRemove(req.params.id)
-  .then(function() {
+router.delete('/:id', authenticate, function(req, res, next) {
+  var fileCabinet = currentUser.fileCabinet.id(req.params.id);
+  if (!fileCabinet) return next(makeError(res, 'Document not found', 404));
+  var index = currentUser.fileCabinet.indexOf(fileCabinet);
+  currentUser.fileCabinet.splice(index, 1);
+  currentUser.save()
+  .then(function(saved) {
     res.redirect('/filecabinet');
   }, function(err) {
     return next(err);
